489c61ab415f9be7d244cbd1ff0b2b8427797ff9
Jerling <1161676899@qq.com>
2018-08-21 22:47:09 +0800

    add 3.5.hpp

--- test.cpp
+++ test.cpp
@@ -2,8 +2,11 @@
 
 int main(void)
 {
-    std::string str1("11");
+    std::string str1("12332111");
     std::string str2("1");
-    std::cout << MySolution().addBinary(str1,str2);
+    std::cout << MySolution().longestPalindromicSubstring(str1) << std::endl;
+    std::cout << Solution_1().longestPalindrome(str1) << std::endl;
+    std::cout << Solution_2().longestPalindrome(str1) << std::endl;
+    std::cout << Solution_3().longestPalindrome(str1) << std::endl;
     return 0;
 }
