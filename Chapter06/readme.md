e12a417c0f366d649e692c4a655bd87eae711ddc
Jerling <1161676899@qq.com>
2018-10-06 15:39:00 +0800

    add 6.2.1.hpp

--- Chapter06/readme.md
+++ Chapter06/readme.md
@@ -6,3 +6,15 @@
 Sort a linked list using insertion sort.
 
 答案：[C++](code/6.1.1.hpp)
+
+## 6.2 归并排序
+### 6.2.1 Merge Two Sorted Arrays
+描述
+
+Given two sorted integer arrays A and B, merge B into A as one sorted array.
+
+Note: You may assume that A has enough space to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
+
+题意：将两个有序的数组合并为一个
+
+答案：[C++](code/6.2.1.hpp)
--- test.cpp
+++ test.cpp
@@ -2,21 +2,18 @@
 
 int main(void)
 {
-
-    TreeNode *t1 = new TreeNode(1);
-    TreeNode *t2 = new TreeNode(2);
-    TreeNode *t3 = new TreeNode(3);
-    TreeNode *t4 = new TreeNode(4);
-    TreeNode *t5 = new TreeNode(5);
-    t2->left = t1;
-    t2->right = t3;
-    t1->left = t5;
-    t1->right = t4;
-    std::vector<std::vector<int> > result;
-    result = Solution().pathSum(t2, 5);
-    for(auto i : result){
-        for( auto v : i)
-            std::cout << v << "\n";
+    ListNode* t1 = new ListNode(1);
+    ListNode* t2 = new ListNode(5);
+    ListNode* t3 = new ListNode(7);
+    ListNode* t4 = new ListNode(6);
+    ListNode* t5 = new ListNode(8);
+    ListNode* t6 = new ListNode(9);
+    t1->next = t2;t2->next = t3;
+    t4->next = t5;t5->next = t6;
+    auto tmp = MySolution().MergeTwoSortedArrays(t1, t4);
+    while(tmp){
+        std::cout << tmp->val << "\n";
+        tmp=tmp->next;
     }
     return 0;
 }
