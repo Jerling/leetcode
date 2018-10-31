91c41f6089987293173e6a8dd5b07c15d8d37407
Jerling <1161676899@qq.com>
2018-11-01 00:31:52 +0800

    add 7.5.hpp

--- test.cpp
+++ test.cpp
@@ -2,8 +2,10 @@
 
 int main(void)
 {
-    std::vector<int> v{8, 9, 10, 5, 6, 8, 8, 8, 8, 8};
-    int r = MySolution().search_in_rotated_sorted_array(v, 10);
-    std::cout << r << "\n";
+    std::vector<int>v{1,   3,  5,  7};
+    std::vector<int>v1{10, 11, 16, 20};
+    std::vector<int> v2{23, 30, 34, 50};
+    std::vector<std::vector<int> >mat{v, v1, v2};
+    std::cout << MySolution().search_a_2D(mat, 3) << std::endl;
     return 0;
 }
