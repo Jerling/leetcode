528711d6ca4ca8a7d76650554f8eeea19ace10a0
Jerling <1161676899@qq.com>
2018-11-02 00:24:32 +0800

    add 7.7.hpp

--- test.cpp
+++ test.cpp
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    std::vector<int>v{1,   4,  7, 11, 15};
+    std::vector<int>v{7, 11, 15, 2, 5, 6};
     std::vector<int>v1{2,   5,  8, 12, 19};
     std::vector<int> v2{3,   6,  9, 16, 22};
     std::vector<std::vector<int> >mat{v, v1, v2};
-    std::cout << MySolution().search_a_2D_2(mat, 1) << std::endl;
+    std::cout << MySolution_2().min_element(v) << std::endl;
     return 0;
 }
